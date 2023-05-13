033245512db643a95729fb899da6609924a10f64
Maria Arruda <Maria.Arruda@adidas.com>
2023-05-13 15:27:23 +0200

    Add SwaggerUI

--- Program.cs
+++ Program.cs
@@ -29,16 +29,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors(options => {
     options.AllowAnyMethod();
     options.AllowAnyHeader(); 
+    options.AllowAnyOrigin(); 
 });
 
-// app.UseSwagger();
-// app.UseSwaggerUI((options) => {
-//     options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
-//     options.RoutePrefix = string.Empty;
-// });
+
 
 
 app.UseHttpsRedirection();
